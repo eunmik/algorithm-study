3210f7403feee2188c60cc57e347025dc9c74a57
eunmik1022 <eunmik1022@gmail.com>
2021-03-31 21:40:43 +0900

    Greedy - Uniform

---
+++ src/main/java/com/eunmi/algorithm/greedy/JoyStick.java
@@ -0,0 +1,15 @@
+package com.eunmi.algorithm.greedy;
+/** https://programmers.co.kr/learn/courses/30/lessons/42860
+ * 제한 사항
+ * name은 알파벳 대문자로만 이루어져 있습니다.
+ * name의 길이는 1 이상 20 이하입니다.
+ * **/
+public class JoyStick {
+    public static void main(String[] args){
+        System.out.println(solution("df"));
+    }
+    public static int solution(String name){
+        int answer = 0;
+        return answer;
+    }
+}
