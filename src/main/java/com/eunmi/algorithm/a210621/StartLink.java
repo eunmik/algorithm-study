94b03283a3d3df37df54cda97744a88e04417f41
eunmik1022 <eunmik1022@gmail.com>
2021-07-05 17:14:44 +0900

    210705-쉬운게 없네

--- src/main/java/com/eunmi/algorithm/a210621/StartLink.java
+++ src/main/java/com/eunmi/algorithm/a210621/StartLink.java
@@ -1,67 +1,90 @@
 package com.eunmi.algorithm.a210621;
 
+import java.util.Scanner;
+
 public class StartLink {
-    public static void main(String... args){
-        StartLink s = new StartLink();
-        s.solution(4);
-//        int[] arr = permutation(new int[]{1,2,3}, 0, 3, 3);
-//        for(int r : arr){
-//            System.out.println(r);
-//        }
-    }
-    public void solution(int size){
-        int[] numbers = new int[size];
-        for(int i=0; i<size; i++){
-            numbers[i] = i+1;
+    static int N;
+    static int[][] map;
+    static boolean[] visit;
+
+    static int Min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+
+        map = new int[N][N];
+        visit = new boolean[N];
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = in.nextInt();
+            }
         }
-        boolean[] visited = new boolean[4];
 
-        //int[] arr = permutation(numbers, 0, numbers.length, numbers.length);
-
+        combi(0, 0);
+        System.out.println(Min);
 
     }
-    public void perm(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
-        if (depth == r) {
-            print(output, r);
+
+    // idx는 인덱스, count는 조합 개수(=재귀 깊이)
+    static void combi(int idx, int count) {
+        // 팀 조합이 완성될 경우
+        if(count == N / 2) {
+			/*
+			 방문한 팀과 방문하지 않은 팀을 각각 나누어
+			 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
+			*/
+            diff();
             return;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (visited[i] != true) {
-                visited[i] = true;
-                output[depth] = arr[i];
-                perm(arr, output, visited, depth + 1, n, r);
-                visited[i] = false;
+        for(int i = idx; i < N; i++) {
+            // 방문하지 않았다면?
+            if(!visit[i]) {
+                visit[i] = true;	// 방문으로 변경
+                combi(i + 1, count + 1);	// 재귀 호출
+                visit[i] = false;	// 재귀가 끝나면 비방문으로 변경
             }
         }
     }
-    public int[] permutation(int[] arr, int depth, int n, int r){
-        if (depth == r){
-            print(arr, r);
-            return arr;
+
+    // 두 팀의 능력치 차이를 계산하는 함수
+    static void diff() {
+        int team_start = 0;
+        int team_link = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                // i 번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수 플러스
+                if (visit[i] == true && visit[j] == true) {
+                    team_start += map[i][j];
+                    team_start += map[j][i];
+                }
+                // i 번째 사람과 j 번째 사람이 false라면 링크팀으로 점수 플러스
+                else if (visit[i] == false && visit[j] == false) {
+                    team_link += map[i][j];
+                    team_link += map[j][i];
+                }
+            }
         }
-        for(int i=depth; i<n; i++){
-            arr = swap(arr, depth, i);
-            permutation(arr, depth + 1, n, r);
-            arr = swap(arr, depth, i);
+        // 두 팀의 점수 차이 (절댓값)
+        int val = Math.abs(team_start - team_link);
+
+        /*
+         * 두 팀의 점수차가 0이라면 가장 낮은 최솟값이기 때문에
+         * 더이상의 탐색 필요없이 0을 출력하고 종료하면 된다.
+         */
+        if (val == 0) {
+            System.out.println(val);
+            System.exit(0);
         }
-        return null;
+
+        Min = Math.min(val, Min);
 
     }
-
-    public int[] swap(int[] arr, int depth, int i){
-        int temp = arr[depth];
-        arr[depth] = arr[i];
-        arr[i] = temp;
-        return arr;
-    }
-
-    // 배열 출력
-    static void print(int[] arr, int r) {
-        for (int i = 0; i < r; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
 
  }
