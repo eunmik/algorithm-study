ad4f2bee9e3f7f6a2233faab8cbfb3752fba2b66
eunmik <eunmik1022@gmail.com>
2021-08-27 11:29:56 +0900

    210827 - [프로그래머스] 카펫 - 리팩토링

--- src/main/java/com/eunmi/algorithm/practices/일요일스터디/A210829/카펫.java
+++ src/main/java/com/eunmi/algorithm/practices/일요일스터디/A210829/카펫.java
@@ -8,56 +8,23 @@ package com.eunmi.algorithm.practices.일요일스터디.A210829;
 public class 카펫 {
     public static void main(String[] args) {
         카펫 c = new 카펫();
-        int[] result = c.solution(8, 1);
+        int[] result = c.solution(24, 24);
         System.out.println(result[0] + " :" + result[1]);
     }
 
     public int[] solution(int brown, int yellow) {
 
-        int[] factors = new int[2];
-        boolean[] visited = new boolean[yellow + 1];
-
         for (int i = 1; i < yellow+1; i++) {
             int factor = yellow;
-            if (i == factor) {
-                factors[0] = i + 2;
-                factors[1] = factor + 2;
+            if (factor % i == 0 && (factor/i+2)*(i+2) - yellow == brown)  {
 
-                int[] result = getBrown(factors, yellow, brown);
-                if(result != null) {
-                    return result;
-                }
-                break;
-            } else {
-                if (factor % i == 0) {
-                    factor = factor / i;
-                    if (!visited[i] && !visited[factor]) {
-
-                        factors[0] = i + 2;
-                        factors[1] = factor + 2;
-                        visited[i] = true;
-                        visited[factor] = true;
-                        int[] result = getBrown(factors, yellow, brown);
-                        if(result != null) {
-                            return result;
-                        }
-                    }
-                }
+                return new int[]{factor / i + 2, i + 2};
             }
         }
+
         return null;
 
     }
-    public int[] getBrown(int[] factors, int yellow, int brown){
-        if ((factors[0] * factors[1]) - yellow == brown) {
-            if (factors[0] < factors[1]) {
-                int tmp = factors[1];
-                factors[1] = factors[0];
-                factors[0] = tmp;
-            }
-            return factors;
-        }
-        return null;
-    }
+
 }
 
