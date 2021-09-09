c5888b74de55c8ccd5d151d652684015997805ce
eunmik <eunmik1022@gmail.com>
2021-09-09 10:59:46 +0900

    210908 - [프로그래머스] 위장

--- src/main/java/com/eunmi/algorithm/practices/일요일스터디/A210905/메뉴리뉴얼.java
+++ src/main/java/com/eunmi/algorithm/practices/일요일스터디/A210905/메뉴리뉴얼.java
@@ -22,11 +22,12 @@ public class 메뉴리뉴얼 {
     static int most;
     public String[] solution(String[] orders, int[] course) {
 
+        //우선순위 큐를 이용한다.
         PriorityQueue<String> pq = new PriorityQueue<>();
         for(int c : course) {
             most = 0;
             Map<String, Integer> map = new HashMap<>();
-            for (int i = 0; i < orders.length; i++) {
+            for (int i = 0; i < orders.length; i++) { //코스요리가 2개일 때, 3개일때, 4개일때 각각 for문을 돌린다.
                 getMenu(0, "", c, 0, orders[i], map);
             }
 
@@ -34,7 +35,7 @@ public class 메뉴리뉴얼 {
 
             while (it.hasNext()) {
                 String key = it.next();
-                if (map.get(key) >= most && most > 1) {
+                if (map.get(key) >= most && most > 1) { //map 안에 있는 메뉴 중에 최대 주문한 갯수인 메뉴만 골라서 큐에 넣어준다. ( 최대 주문한 갯수가 적어도 2개 이상이어야 한다)
                     pq.offer(key);
                 }
             }
@@ -47,14 +48,14 @@ public class 메뉴리뉴얼 {
 
         return result;
     }
-
+    //재귀를 사용해서 코스요리 갯수에 맞게 메뉴를 구성한다.
     public static void getMenu(int cnt, String str, int target, int idx, String word, Map<String, Integer> map){
         if(cnt == target) {
             char[] c = str.toCharArray();
-            Arrays.sort(c);
+            Arrays.sort(c); // "XY" 가 있고 "YX"가 나올 때가 있으니깐 정렬을 해준다.
             str = String.valueOf(c);
-            map.put(str, (map.get(str) == null ? 1 : map.get(str) + 1));
-            if(most < map.get(str)) {
+            map.put(str, (map.get(str) == null ? 1 : map.get(str) + 1)); //맵에 메뉴를 넣고 기존에 값이 있으면 +1을 더해준다.
+            if(most < map.get(str)) { //최대 주문한 갯수를 구한다.
                 most = map.get(str);
             }
             return;
--- src/main/java/com/eunmi/algorithm/practices/일요일스터디/A210905/큰수만들기_재도전.java
+++ src/main/java/com/eunmi/algorithm/practices/일요일스터디/A210905/큰수만들기_재도전.java
@@ -46,21 +46,4 @@ public class 큰수만들기_재도전 {
         return sb.toString().substring(0, number.length()-k);
     }
 
-    public String solution2(String number, int k) {
-        char[] result = new char[number.length() - k];
-        Stack<Character> stack = new Stack<>();
-
-        for (int i=0; i<number.length(); i++) {  // 하나씩 돌면서
-            char c = number.charAt(i);
-            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) { // top보다 큰값이면 pop()!
-                stack.pop();
-            }
-            stack.push(c);
-        }
-        for (int i=0; i<result.length; i++) {
-            result[i] = stack.get(i);
-        }
-        return new String(result);
-    }
-
 }
