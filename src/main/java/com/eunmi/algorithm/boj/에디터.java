122958e421765db38f4b339740706631ee55777e
eunmik <eunmik1022@gmail.com>
2022-04-20 17:02:18 +0900

    220420 - [BOJ][시간초과] 1406 에디터
    List 대신 stack을 사용했지만 여전히 시간초과..

--- src/main/java/com/eunmi/algorithm/boj/에디터.java
+++ src/main/java/com/eunmi/algorithm/boj/에디터.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 /**
  * https://www.acmicpc.net/problem/1406
+ * Hint : stack
  */
 public class 에디터 {
     public static void main(String[] args) throws Exception{
@@ -17,12 +19,12 @@ public class 에디터 {
         String word = br.readLine();
         int N = Integer.parseInt(br.readLine());
 
-        char[] wordArray = word.toCharArray();
-        List<Character> list = new ArrayList<>();
-        for(char w : wordArray){
-            list.add(w);
+        Stack<Character> stack = new Stack<>();
+        for(int i =0; i<word.length(); i++){
+            stack.push(word.charAt(i));
         }
-        int currentCursor = wordArray.length;
+
+        int currentCursor = stack.size();
         while (N > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             N--;
@@ -33,25 +35,25 @@ public class 에디터 {
                     }
                     break;
                 case "D" : //커서를 오른쪽으로 한 칸 옮김 (커서가 문장의 맨 뒤이면 무시됨)
-                    if(currentCursor != list.size()){
+                    if(currentCursor != stack.size()){
                         currentCursor+= 1;
                     }
                     break;
                 case "B" : // 커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨)
                     //삭제로 인해 커서는 한 칸 왼쪽으로 이동한 것처럼 나타나지만, 실제로 커서의 오른쪽에 있던 문자는 그대로임
                     if(currentCursor != 0){
-                        list.remove(currentCursor-1);
+                        stack.remove(currentCursor-1);
                         currentCursor -=1;
                     }
                     break;
                 case "P" : //$라는 문자를 커서 왼쪽에 추가함
-                    list.add(currentCursor, st.nextToken().charAt(0));
+                    stack.add(currentCursor, st.nextToken().charAt(0));
                     currentCursor+=1;
 
                     break;
             }
         }
-        for(char c : list){
+        for(char c : stack){
             sb.append(c);
         }
         System.out.println(sb);
