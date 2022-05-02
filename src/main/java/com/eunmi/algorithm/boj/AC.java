cdd4138fbb1d1b23b3cbdf13e93d30ae413b7887
eunmik <eunmik1022@gmail.com>
2022-05-02 10:59:26 +0900

    220502 - [BOJ][런타임에러] 5430 AC

---
+++ src/main/java/com/eunmi/algorithm/boj/AC.java
@@ -0,0 +1,64 @@
+package com.eunmi.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * https://www.acmicpc.net/problem/5430
+ */
+public class AC{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while(T > 0){
+            char[] commands = br.readLine().toCharArray();
+            int N = Integer.parseInt(br.readLine());
+            List<Integer> list = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            boolean isError = false;
+
+            for(int i =0; i<N; i++){
+                String token = st.nextToken();
+                token = token.replaceAll("[\\[\\]]", "");
+                list.add(Integer.parseInt(token));
+            }
+
+            for(int i =0; i<commands.length; i++){
+                switch(commands[i]){
+                    case 'R':
+                        int tmp = list.get(0);
+                        Collections.sort(list, Collections.reverseOrder());
+                        if (tmp == list.get(0)) {
+                            Collections.sort(list);
+                        }
+
+                        break;
+                    case 'D':
+                        if(list.isEmpty()){
+                            sb.append("error\n");
+                            isError = true;
+                        }else {
+                            list.remove(0);
+                        }
+                        break;
+                }
+            }
+            if(!isError) {
+                sb.append("[");
+                for (int i = 0; i < list.size(); i++) {
+                    sb.append(list.get(i)+",");
+
+                }
+                sb.replace(sb.lastIndexOf(","), sb.length(), "]");
+                sb.append("\n");
+            }
+
+            T--;
+        }
+        System.out.print(sb);
+    }
+
+}
