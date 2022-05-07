9d630fe5d95335ad0593d037ce594e29f8993a6b
eunmik <eunmik1022@gmail.com>
2022-05-07 20:00:37 +0900

    220507 - [BOJ] 1182 부분수열의 함
    로또(6603)에서 풀었던 것처럼 dfs와 백트랙킹을 사용해서 푸니깐 바로 성공!

---
+++ src/main/java/com/eunmi/algorithm/boj/부분수열의합.java
@@ -0,0 +1,45 @@
+package com.eunmi.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1182
+ */
+public class 부분수열의합 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        cnt = 0;
+        array = new int[N];
+        checked = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i =0; i<N; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(0, 0, 0);
+        System.out.println(cnt);
+    }
+    static int cnt;
+    static int[] checked;
+    static int[] array;
+    static int N;
+    static int S;
+
+    public static void dfs(int start, int depth, int sum){
+        if(sum == S && depth > 0){
+            cnt++;
+        }
+
+        for(int i =start; i<N; i++){
+            checked[i] = 1;
+            sum += array[i];
+            dfs(i+1, depth+1, sum);
+            checked[i] = 0;
+            sum -= array[i];
+        }
+    }
+}
