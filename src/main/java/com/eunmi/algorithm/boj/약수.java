64b2d8fc3c690afbb584877319914f0067ebc174
eunmik <eunmik1022@gmail.com>
2022-05-11 15:18:00 +0900

    220511 - [BOJ][틀렸습니다] 1037 약수

---
+++ src/main/java/com/eunmi/algorithm/boj/약수.java
@@ -0,0 +1,26 @@
+package com.eunmi.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1037
+ * 약수는 영어로 divisor
+ */
+public class 약수 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] divisor = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i =0; i<N; i++){
+            divisor[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(divisor);
+        System.out.println(divisor[N-1] * 2);
+
+
+    }
+}
