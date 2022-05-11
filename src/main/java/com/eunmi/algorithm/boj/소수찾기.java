187595cdec5143f5ef77bd3d0c79d525fd8c8a08
eunmik <eunmik1022@gmail.com>
2022-05-11 18:08:06 +0900

    220511 - [BOJ][틀렸습니다] 1978 소수찾기

---
+++ src/main/java/com/eunmi/algorithm/boj/소수찾기.java
@@ -0,0 +1,24 @@
+package com.eunmi.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1978
+ */
+public class 소수찾기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
+            if(num != 1 && num % 2 != 0) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
