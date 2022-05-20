43ca75d237037d995b062fe3767d0d4c25c1bfc6
eunmik <eunmik1022@gmail.com>
2022-05-20 11:40:27 +0900

    220520 - [BOJ][틀렸습니다] 2661 좋은 수열
    좋은 수열을 가려내는것도 어렵고.. 백트랙킹도 어렵고.. ㅠㅠ

---
+++ src/main/java/com/eunmi/algorithm/boj/좋은수열.java
@@ -0,0 +1,65 @@
+package com.eunmi.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://www.acmicpc.net/problem/2661
+ */
+public class 좋은수열 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        result = Integer.MAX_VALUE;
+        sequence = new int[N];
+        dfs(0, 0, 1);
+        System.out.println(result);
+    }
+    static int N;
+    static int result;
+    static int[] sequence;
+
+    static void dfs(int index, int depth, int num){
+        if(depth == N){
+            if(isGoodSequence()){
+                String tmp = "";
+                for(int i =0; i<sequence.length; i++){
+                    tmp += sequence[i];
+                }
+                result = Math.min(Integer.parseInt(tmp), result);
+            }
+            return;
+        }
+        for(int i = index; i<N; i++){
+            if(num > 3) num = 1;
+            sequence[i] = num;
+            dfs(i+1, depth+1, num+1);
+            //sequence[i] = 0;
+        }
+    }
+
+    static boolean isGoodSequence(){
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i =0; i<N; i++){
+            if(queue.isEmpty()) {
+                queue.add(sequence[i]);
+            }
+            else {
+                int size = queue.size();
+                while(size > 0) {
+                    int top = queue.poll();
+                    if (top == sequence[i]) {
+                        return false;
+                    } else {
+                        queue.add(top);
+                        size--;
+                    }
+                }
+                queue.add(sequence[i]);
+            }
+        }
+        return true;
+    }
+}
