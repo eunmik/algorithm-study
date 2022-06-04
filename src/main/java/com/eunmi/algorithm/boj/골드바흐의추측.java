a3724d076b82edc2ed84dd5a6edd8a083138b34d
eunmik <eunmik1022@gmail.com>
2022-06-04 21:48:10 +0900

    220604 - [BOJ][다른사람풀이]6588 골드바흐의 추측

--- src/main/java/com/eunmi/algorithm/boj/골드바흐의추측.java
+++ src/main/java/com/eunmi/algorithm/boj/골드바흐의추측.java
@@ -12,39 +12,23 @@ import java.util.List;
 public class 골드바흐의추측 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> prime_list = sieve(1000000);
+        boolean[] prime = sieve(1000000);
         while(true){
             int num = Integer.parseInt(br.readLine());
             if(num == 0){
                 break;
             }
 
-            int a = 0, b = 0;
-            int n = 0;
-
-            for(int i =0; i<prime_list.size()/2; i++){
-                int num1 = prime_list.get(i);
-                for(int j =i+1; j<prime_list.size()/2; j++){
-                    int num2 = prime_list.get(j);
-                    int total = num1 + num2;
-                    if(total == num){
-                            a = num1;
-                            b = num2;
-                            n = num2 - num1;
-                            break;
-                    }
-                }
-                if(n > 0){
+            for(int i =2; i<=num/2; i++){
+                if(!prime[i] && !prime[num-i]){
+                    System.out.println(num +" = "+ i +" + "+ (num-i));
                     break;
                 }
             }
-            System.out.println(num +" = "+a+" + "+b);
-
-
         }
     }
 
-    static List<Integer> sieve(int n){
+    static boolean[] sieve(int n){
         boolean prime[] = new boolean[n+1];
         List<Integer> prime_number = new ArrayList<>();
         prime[0] = prime[1] = true;
@@ -60,6 +44,6 @@ public class 골드바흐의추측 {
                 prime_number.add(i);
             }
         }
-        return prime_number;
+        return prime;
     }
 }
