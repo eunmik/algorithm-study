168f6334330d4b3abee8163abf0a6e3f08970c73
eunmik <eunmik1022@gmail.com>
2022-06-04 21:46:38 +0900

    220604 - [BOJ][런타임에러]15711 환상의 짝꿍
    다른 사람 풀이 보고 했는데도 왜 런타임 에러 (NumberFormat) 날까..ㅠ

--- src/main/java/com/eunmi/algorithm/boj/환상의짝꿍.java
+++ src/main/java/com/eunmi/algorithm/boj/환상의짝꿍.java
@@ -2,6 +2,8 @@ package com.eunmi.algorithm.boj;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -9,10 +11,14 @@ import java.util.StringTokenizer;
  * Hint : 골드바흐의 추측, 에라토스테네스의 체
  */
 public class 환상의짝꿍 {
+    static boolean[] prime;
+    static List<Integer> list;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        boolean[] prime = sieve();
+        list = new ArrayList<>();
+
+        sieve();
         StringBuilder sb = new StringBuilder();
         while (N > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -20,22 +26,14 @@ public class 환상의짝꿍 {
             int b = Integer.parseInt(st.nextToken());
             int num = a + b;
             if (num % 2 == 0 && num >= 4) { //골드바흐의 추측에 따르면 4이상의 짝수는 두 소수의 합으로 이루어진다. 그러므로 4 이상의 짝수는 YES
-                sb.append("YES");
+                sb.append("YES\n");
             } else if (num < 4) {
-                sb.append("NO");
+                sb.append("NO\n");
             } else { //홀수 일 때
-                boolean isTrue = false;
-                for (int i = 2; i < num / 2; i++) {
-                    if (!prime[i] && !prime[num - i]) {
-                        System.out.println(i + " : " + (num - i));
-                        isTrue = true;
-                        break;
-                    }
-                }
-                if(isTrue){
-                    sb.append("YES");
+                if(check(num)){
+                    sb.append("YES\n");
                 }else {
-                    sb.append("NO");
+                    sb.append("NO\n");
                 }
             }
             N--;
@@ -43,9 +41,21 @@ public class 환상의짝꿍 {
         System.out.println(sb);
     }
 
-    static boolean[] sieve() {
+    public static boolean check(long x) {
+        if (x <= 2_000_000) return !prime[(int) x];
+
+        for (int i = 0; i < list.size(); i++) {
+            if (x % list.get(i) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void sieve() {
         int n = 2000000;
-        boolean[] prime = new boolean[n + 1];
+        prime = new boolean[n + 1];
         prime[0] = prime[1] = true;
         for (int i = 2; i * i < n; i++) {
             if (!prime[i]) {
@@ -54,6 +64,11 @@ public class 환상의짝꿍 {
                 }
             }
         }
-        return prime;
+        for(int i =0; i<prime.length; i++){
+            if(!prime[i]) {
+                list.add(i);
+            }
+        }
+
     }
 }
